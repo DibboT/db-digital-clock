e01141301db491bec8d3f74bb9ce8c12aa8c9401
Dibbo T <dibbotripura7@gmail.com>
2023-09-30 10:00:19 +0600

    index js file has been updated

---
+++ index.js
@@ -0,0 +1,28 @@
+function clock(){
+
+    let hour = document.getElementById('hour');
+    let minute = document.getElementById('minute');
+    let seconds =  document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    var am = "AM";
+
+//Convert 24 hour time to 12 hour format with AM PM indicator
+
+  if (h >12){
+    h=  h - 12;
+    var am = 'PM';
+
+  }
+  
+
+    hour.innerHTML = h;
+    minute.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+
+}
+var interval = setInterval(clock, 1000);
